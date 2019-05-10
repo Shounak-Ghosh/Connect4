66374478d6c76ef27fd001f4df2c9d93c20b1033
Shounak Ghosh <43559571+Shounak-Ghosh@users.noreply.github.com>
2019-05-09 22:36:27 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,9 +1,19 @@
-import java.util.*;
 
-/**
- * Represents the game board
- */
 public class Board
 {
-   
+    
+
+    public boolean isValid(Location dest)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    
+    public Object get(Location dest)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
