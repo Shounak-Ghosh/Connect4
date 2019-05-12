106722b21c2c3a20779d0931316c202a3c5310a9
Shounak Ghosh <43559571+Shounak-Ghosh@users.noreply.github.com>
2019-05-11 23:27:40 -0700

    Update GameDisplay.java

--- GameDisplay.java
+++ GameDisplay.java
@@ -5,136 +5,198 @@ import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 
-public class GameDisplay extends Display {
-    
+public class GameDisplay extends Display
+{
+
     Graphics g;
     private boolean gameIsOver;
-    
-    public GameDisplay(Player p1, Player p2) {
+    private Player currentPlayer;
+    private Player player1;
+    private Player player2;
+
+    // random/digital player is assumed to be in the p2 position
+    public GameDisplay(Player p1, Player p2)
+    {
         this.board = new Board(p1, p2);
+        currentPlayer = p1;
+        player1 = p1;
+        player2 = p2;
         gameIsOver = false;
     }
-    
-    public void play() {
-        
+
+    public void play()
+    {
+        System.out.println("Reached play");
+        int move = 0;
+        while (!gameIsOver)
+        {
+            System.out.println("current: " + currentPlayer);
+            if (currentPlayer.equals(player2))
+            {
+                System.out.println("Reached random players turn");
+                move = player2.getMove();
+                System.out.println(move);
+                while(!board.isValidMove(move)) 
+                {
+                    move = player2.getMove();
+                    System.out.println(move);
+                }
+                board.makeMove(move);
+                repaint();
+                currentPlayer = player1;
+            }
+            
+//            try
+//            {
+//                Thread.sleep(1000);
+//            }
+//            catch (InterruptedException e)
+//            {
+//                e.printStackTrace();
+//            }
+        }
     }
 
-    public void mouseClicked(MouseEvent e) {
-        if (!gameIsOver) { 
+    public void mouseClicked(MouseEvent e)
+    {
+        if (!gameIsOver)
+        {
             // game is not over, the click represents a move
-                                    
+
             int column = getColumn(e.getX());
-            
+
             // if the clicked column can be moved in
-            if (board.isValidMove(column)) {
-                
+            if (board.isValidMove(column))
+            {
                 board.makeMove(column); // makes the move
                 repaint(); // repaints the window to reflect the move
-                
+
                 // tests for a winner - if there is, ends the game
                 // mouse clicks will have different functions
                 Player winner = board.winner();
-                if (winner!=null) {
+                if (winner != null)
+                {
                     System.out.println("WINNER!");
                     gameIsOver = true;
                 }
-                
-                
+                currentPlayer = player2;
+                System.out.println("current: " + currentPlayer);
             }
-        } else {
+        }
+        else
+        {
             // someone has already won the game, no more moves can be made
             System.out.println("no more...");
         }
-        
+
     }
-    
+
     public void paintComponent(Graphics g)
     {
         System.out.println("repainting");
-                
+
         paintGrid(g);
         paintPieces(g);
         paintSidebar(g);
     }
-    
+
     // TODO: WRITE THIS
     // SHOULD HAVE: THE TWO PLAYERS - THEIR COLORS & NAMES. THE ONE WHOSE TURN IT IS
-    // SHOULD BE HIGHLIGHTED. ALSO HAVE A MAIN MENU BUTTON FOR RETURNING TO MAIN MENU
-    private void paintSidebar(Graphics g) {
+    // SHOULD BE HIGHLIGHTED. ALSO HAVE A MAIN MENU BUTTON FOR RETURNING TO MAIN
+    // MENU
+    private void paintSidebar(Graphics g)
+    {
         g.setColor(board.getPlayer1().getColor());
         g.fillOval(640, 50, 70, 70);
         g.setColor(board.getPlayer2().getColor());
         g.fillOval(640, 175, 70, 70);
-        
+
         g.setColor(Color.BLACK);
-       
+
     }
-    
+
     /**
-     * Paints the entire screen the grid color first, then paints
-     * each empty "square" icon on. Does this so that it is easy
-     * to give the empty slots rounded corners.
+     * Paints the entire screen the grid color first, then paints each empty
+     * "square" icon on. Does this so that it is easy to give the empty slots
+     * rounded corners.
+     * 
      * @param g
      */
-    private void paintGrid(Graphics g) {
+    private void paintGrid(Graphics g)
+    {
         frame.getContentPane().setBackground(GRID_COLOR);
-        
+
         g.setColor(BACKGROUND_COLOR);
-        
+
         // adding in the empty slots
-        for (int c=0;c<=6;c++) {
-            for (int r=0;r<=5;r++) {
-                paintSlot(g,c,r,BACKGROUND_COLOR);
+        for (int c = 0; c <= 6; c++)
+        {
+            for (int r = 0; r <= 5; r++)
+            {
+                paintSlot(g, c, r, BACKGROUND_COLOR);
             }
         }
     }
-        
+
     /**
      * Paints a single slot in a given color, with provided row AND COLUMN.
-     * @param g the graphics painter
-     * @param c the column of the slot to be painted
-     * @param r the row of the slot to be painted
+     * 
+     * @param g     the graphics painter
+     * @param c     the column of the slot to be painted
+     * @param r     the row of the slot to be painted
      * @param color
      */
-    private void paintSlot(Graphics g, int c, int r, Color color) {
+    private void paintSlot(Graphics g, int c, int r, Color color)
+    {
         g.setColor(color);
-        g.fillRoundRect(c * 75 + (c+1) * 10, 
-                r * 75 + (r+1) * 10, 
-                75, 75, 45, 45);
+        g.fillRoundRect(c * 75 + (c + 1) * 10, r * 75 + (r + 1) * 10, 75, 75, 45, 45);
     }
-    
+
     /**
-     * Goes through board.getPieces, and where there is a piece,
-     * use g to draw a circle with the piece's color. 
+     * Goes through board.getPieces, and where there is a piece, use g to draw a
+     * circle with the piece's color.
+     * 
      * @param g the graphics painter
      */
-    private void paintPieces(Graphics g) {
+    private void paintPieces(Graphics g)
+    {
         Piece[][] piece = board.getPieces();
-        
-        for(int c=0;c<piece[0].length;c++) {
-            for(int r=0;r<piece.length;r++) {
-                if (piece[r][c]!=null) {
-                    paintSlot(g,c,r,piece[r][c].getColor());
+
+        for (int c = 0; c < piece[0].length; c++)
+        {
+            for (int r = 0; r < piece.length; r++)
+            {
+                if (piece[r][c] != null)
+                {
+                    paintSlot(g, c, r, piece[r][c].getColor());
                 }
             }
         }
     }
-    
+
     /**
-     * Returns the column of the game that the given x-coordinate
-     * would be in.
-     * @param x the x-coordinate 
+     * Returns the column of the game that the given x-coordinate would be in.
+     * 
+     * @param x the x-coordinate
      * @return the column
      */
-    private int getColumn(int x) {
-        if (x>=10 && x<=85) return 0;
-        else if (x>=95 && x<=170) return 1;
-        else if (x>=180 && x<=255) return 2;
-        else if (x>=265 && x<=340) return 3;
-        else if (x>=350 && x<=425) return 4;
-        else if (x>=435 && x<=510) return 5;
-        else if (x>=520 && x<=595) return 6;
+    private int getColumn(int x)
+    {
+        if (x >= 10 && x <= 85)
+            return 0;
+        else if (x >= 95 && x <= 170)
+            return 1;
+        else if (x >= 180 && x <= 255)
+            return 2;
+        else if (x >= 265 && x <= 340)
+            return 3;
+        else if (x >= 350 && x <= 425)
+            return 4;
+        else if (x >= 435 && x <= 510)
+            return 5;
+        else if (x >= 520 && x <= 595)
+            return 6;
         return -1;
     }
-    
+
 }
