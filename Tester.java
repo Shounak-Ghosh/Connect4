53ee6016b0bc8b2b1e95c6e6c8608a225a66f89d
g10ria <22gloriaz@students.harker.org>
2019-05-12 23:02:03 -0700

    integrated randomplayer into gamedisplay, changed tester

--- Tester.java
+++ Tester.java
@@ -7,7 +7,7 @@ public class Tester
     public static void main(String[] args)
     {
         Player p1 = new HumanPlayer("Player 1", Color.YELLOW);
-        Player p2 = new RandomPlayer("Player 2");
+        Player p2 = new RandomPlayer("Player 2", Color.RED);
         Display d = new GameDisplay(p1, p2);
     }
 }
