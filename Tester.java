8ac03f25a518d2c848f81acf50313a54a99b88da
Shounak Ghosh <43559571+Shounak-Ghosh@users.noreply.github.com>
2019-05-12 18:13:14 -0700

    Update Tester.java
    changed colors

--- Tester.java
+++ Tester.java
@@ -2,10 +2,12 @@ package Connect4;
 
 import java.awt.Color;
 
-public class Tester {
-    public static void main(String[] args) {
+public class Tester
+{
+    public static void main(String[] args)
+    {
         Player p1 = new HumanPlayer("Player 1", Color.RED);
-        Player p2 = new HumanPlayer("Player 2", Color.BLUE);
-        Display d = new GameDisplay(p1, p2);        
+        Player p2 = new HumanPlayer("Player 2", Color.YELLOW);
+        Display d = new GameDisplay(p1, p2);
     }
 }
