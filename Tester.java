44c7e70cf69e6998edba62692c3b0aa94e6bbc62
Shounak Ghosh <43559571+Shounak-Ghosh@users.noreply.github.com>
2019-05-14 16:02:26 -0700

    changed tester to include another HumanPlayer

--- Tester.java
+++ Tester.java
@@ -7,7 +7,10 @@ public class Tester
     public static void main(String[] args)
     {
         Player p1 = new HumanPlayer("Player 1", Color.YELLOW);
-        Player p2 = new RandomPlayer("Player 2", Color.RED);
-        Display d = new GameDisplay(p1, p2);
+        Player p2 = new HumanPlayer("Player 2", Color.RED);
+        Player r1 = new RandomPlayer("Random Player", Color.RED);
+        
+        
+        Display d = new GameDisplay(p1, r1);
     }
 }
