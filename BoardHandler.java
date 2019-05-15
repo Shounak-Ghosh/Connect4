2090440f3c52a534c9519b9192808d0ae1023ce6
g10ria <22gloriaz@students.harker.org>
2019-05-15 09:50:34 -0700

    minor changes

--- BoardHandler.java
+++ BoardHandler.java
@@ -2,6 +2,7 @@ package Connect4;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 
@@ -28,38 +29,6 @@ public class BoardHandler extends Display
         computerized = (p2 instanceof RandomPlayer || p2 instanceof DefensivePlayer);
         lastColumn = -1;
     }
-
-//    public void activate()
-//    {
-//        System.out.println("Reached play");
-//        int move = 0;
-//        while (!gameIsOver)
-//        {            
-//            if (!board.isHumanTurn())
-//            {
-//                System.out.println("Reached random players turn");
-//                move = player2.getMove();
-//                System.out.println(move);
-//                while(!board.isValidMove(move)) 
-//                {
-//                    move = player2.getMove();
-//                    System.out.println(move);
-//                }
-//                board.makeMove(move);
-//                repaint();
-//                currentPlayer = player1;
-//            }
-//            
-////            try
-////            {
-////                Thread.sleep(1000);
-////            }
-////            catch (InterruptedException e)
-////            {
-////                e.printStackTrace();
-////            }
-//        }
-//    }
 
     /**
      * Handles an entire set of moves in the game DOCUMENT THIS 
@@ -108,7 +77,8 @@ public class BoardHandler extends Display
     }
     
     private boolean isUndo(int xCoord, int yCoord) {
-        return xCoord > 400;
+        System.out.println(xCoord+" "+yCoord);
+        return (yCoord>=400 && yCoord<=440 && xCoord>=630 && xCoord<=715);
     }
     
    
@@ -208,9 +178,18 @@ public class BoardHandler extends Display
         g.setColor(board.getPlayer2().getColor());
         g.fillOval(640, 175, 70, 70);
 
-        g.setColor(Color.WHITE);
+        g.setColor(TEXT_COLOR);
+        g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
         g.drawString(p1.name, 630, 30);
         g.drawString(p2.name, 630, 155);
+        
+        g.drawRect(630, 400, 85, 40);
+        
+        g.drawRect(630, 460, 85, 40);
+        
+        g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 22));
+        g.drawString("UNDO", 640, 429);
+        g.drawString("BACK", 643, 489);
 
     }
 
@@ -256,6 +235,8 @@ public class BoardHandler extends Display
                 75, 75, 45, 45);
         g.drawRoundRect(c * 75 + (c + 1) * 10 + 1, r * 75 + (r + 1) * 10 + 1, 
                 73, 73, 45, 45);
+        g.drawRoundRect(c * 75 + (c + 1) * 10 + 2, r * 75 + (r + 1) * 10 + 2, 
+                71, 71, 45, 45);
     }
     
     /**
--- Display.java
+++ Display.java
@@ -16,7 +16,8 @@ public abstract class Display extends JComponent implements MouseListener
     
     protected final Color BACKGROUND_COLOR = new Color(192,192,192);
     protected final Color GRID_COLOR = Color.BLUE;
-    protected final Color HIGHLIGHT_COLOR = Color.white;
+    protected final Color HIGHLIGHT_COLOR = Color.GREEN;
+    protected final Color TEXT_COLOR = Color.WHITE;
     
     // insets of the frame (platform-dependent)
     protected Insets insets;
