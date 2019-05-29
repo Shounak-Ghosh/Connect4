884c9f70871e835373be6ab678fa126c7783451a
Michael <michael@tests-MacBook-Pro.local>
2019-05-29 11:48:29 -0700

    modified display, created Location class

--- GoDisplay.java
+++ GoDisplay.java
@@ -2,16 +2,27 @@ package Connect4;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class GoDisplay extends Display
 {
@@ -20,307 +31,258 @@ public class GoDisplay extends Display
     private boolean gameIsOver;
     private boolean computerized; // true if the game is against the computer
                                   // false if it's between 2 humans
-    
+    private GoBoard board;
+    private JButton[][] grid;
+    private JFrame frame;
+    private Color[][] colors;
     Player p1;
     Player p2;
     
 
     public GoDisplay(Player p1, Player p2)
     {
-        this.board = new Board(p1, p2);
+        this.board = new GoBoard(p1, p2);
         gameIsOver = false;
         this.p1 = p1;
         this.p2 = p2;
+        grid = new JButton[19][19];
+        colors = new Color[19][19];
 
         // change this for digitalplayer
         // note: p2 will always be the computer bc we give human first turn
-        computerized = (p2 instanceof RandomPlayer || p2 instanceof DefensivePlayer);
-        
-        
-        
-        
+        computerized = !(p2 instanceof GoHumanPlayer);//make later
+    }
+
+    
+    
+    //break for now
+    
+   
+
+    /**
+     * 
+     * Constructs a new display for displaying the given board
+     * @param board the board
+     */ 
+    public BoardDisplay(Board board)
+    {
+        this.board = board;
+        grid = new JButton[board.getNumRows()][board.getNumCols()];
+        colors = new Color[board.getNumRows()][board.getNumCols()];
+
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable()
+            {
+                public void run()
+                {
+                    createAndShowGUI();
+                }
+            });
+
+        //Wait until display has been drawn
+        try
+        {
+            while (frame == null || !frame.isVisible())
+                Thread.sleep(1);
+        }
+        catch(InterruptedException e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     /**
-     * Handles an entire set of moves in the game DOCUMENT THIS
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
      */
-    public void mouseClicked(MouseEvent e)
+    private void createAndShowGUI()
     {
-        int xCoord = e.getX();
-        int yCoord = e.getY();
+        //Create and set up the window.
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new GridLayout(board.getNumRows(), board.getNumCols()));
 
-        if (isUndo(xCoord - insets.left, yCoord - insets.top))
-        {
-            board.undo();
-            repaint();
-            return;
-        }
-
-        // Human player is yellow, computer is red
-        if (!gameIsOver && board.isHumanTurn()) // game is not over, the click represents a move
-        {
-            int column = getColumn(xCoord - insets.left);
-
-            if (makeMove(column) && computerized && !gameIsOver) // it is the non-human players move
+        //Create each square as a button.
+        for (int row = 0; row < grid.length; row++)
+            for (int col = 0; col < grid[row].length; col++)
             {
-//                rest();
-
-                // SLEEP HERE - FIGURE THIS OUT FOR DIGITALPLAYER
-
-                if (p2 instanceof RandomPlayer)
-                {
-                    makeMove(randomPlayerMove());
-                }
-                else if (p2 instanceof DefensivePlayer)
-                {
-                    makeMove(defensivePlayerMove());
-                }
-                // makeMove(randomPlayerMove());
-                // board.undo();
-                
+                grid[row][col] = new JButton();
+                grid[row][col].setOpaque(true);
+                if ((row + col) % 2 == 0)
+                    grid[row][col].setBackground(new Color(155, 145, 115));
+                else
+                    grid[row][col].setBackground(new Color(110, 85, 55));
+                grid[row][col].setPreferredSize(new Dimension(50, 50));
+                grid[row][col].setActionCommand(row + "," + col);
+                grid[row][col].addActionListener(this);
+                frame.getContentPane().add(grid[row][col]);
             }
 
+        //Show the pieces
+        showBoard();
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    /**
+     * 
+     *Called when a square is clicked.
+     * @param event the event
+     */
+    public void actionPerformed(ActionEvent event)
+    {
+        //Determine location of clicked button.
+        String command = event.getActionCommand();
+        int comma = command.indexOf(",");
+        int row = Integer.parseInt(command.substring(0, comma));
+        int col = Integer.parseInt(command.substring(comma + 1));
+        Location loc = new Location(row, col);
+
+        if (selectedPiece == null)
+        {
+            //we have just selected a piece for the first time.
+            selectedPiece = board.get(loc);
+            clearColors();
+            if (selectedPiece != null)
+            {
+                setColor(loc, Color.YELLOW);
+                for(Location location: selectedPiece.destinations())
+                {
+                    setColor(location, Color.YELLOW);
+                }
+            }
+        }
+        else if (loc.equals(selectedPiece.getLocation()))
+        {
+            //we are deselecting the piece
+            selectedPiece = null;
+            selectedMove = null;
+            clearColors();
+        }
+        else if(board.get(loc)!=null&&selectedPiece.getColor().equals(board.get(loc).getColor()))
+        {
+            selectedPiece = board.get(loc);
+            clearColors();
+            if (selectedPiece != null)
+            {
+                setColor(loc, Color.YELLOW);
+                ArrayList<Location> lo = selectedPiece.destinations();
+                Iterator<Location> it = lo.iterator();
+                while(it.hasNext())
+                {
+                    setColor(it.next(), Color.YELLOW);
+                }
+            }
         }
         else
         {
-            // someone has already won the game, no more moves can be made
-            System.out.println("Game has ended.");
-            board.restart();
-            gameIsOver = false;
-            board.setCurrentPlayer(p1);
-            repaint();
+            //we have selected a move
+            selectedMove = new Move(selectedPiece, loc);
+            setColor(loc, Color.YELLOW);
         }
 
+        //         ArrayList<Move> locas = board.allMoves(Color.BLACK);
+        //         Iterator<Move> it = locas.iterator();
+        //         while(it.hasNext())
+        //         {
+        //             setColor(it.next().getDestination(), Color.YELLOW);
+        //         }
     }
 
-    private boolean isUndo(int xCoord, int yCoord)
+    /**
+     * 
+     *Redraws the board to include the pieces and border colors.
+     */
+    public void showBoard()
     {
-        System.out.println(xCoord + " " + yCoord);
-        return (yCoord >= 400 && yCoord <= 440 && xCoord >= 630 && xCoord <= 715);
-    }
-
-    private int randomPlayerMove()
-    {
-        int move = (int) (Math.random() * 19);
-        while (!board.isValidMove(move))
-        {
-            move = (int) (Math.random() * 19);
-        }
-        return move;
-    }
-
-    // TODO: make a twoInARow method in board (similar to winner) and use that
-    // instead
-    private int defensivePlayerMove()
-    {
-        System.out.println("----------------------------------");
-        
-        for (int i = 0; i < 7; i++)
-        {
-            board.setCurrentPlayer(p2);
-            if (makeTempMove(i, Color.RED)) // computer makes a move
+        for (int row = 0; row < grid.length; row++)
+            for (int col = 0; col < grid[row].length; col++)
             {
-                
-                for (int j = 0; j < 7; j++)
-                {
-                    board.setCurrentPlayer(p1); // set player to human player
-                    if (makeTempMove(j, Color.YELLOW)) // human makes a move
-                    {
-                        
-                        if (board.winner() != null && board.winner().equals(p1)) // if the human wins on this move
-                        {
-                            System.out.println("MOVE TO BLOCK");
-                            board.undo();
-                            board.undo();
-                            board.setCurrentPlayer(p2); // set player to computer
-                            return j; // blocks the move from occurring
-                        }
-                    }
-                    board.undo();
-                }
-            }
-            board.undo();
-        }
+                Location loc = new Location(row, col);
 
-        board.setCurrentPlayer(p2);
-        return randomPlayerMove();
+                Piece piece = board.get(loc);
+
+                Icon icon = null;
+                if (piece != null)
+                {
+                    //System.out.println(loc);
+                    grid[row][col].setForeground(piece.getColor());
+                    icon = new ImageIcon(piece.getImageFileName());
+                }
+                grid[row][col].setIcon(icon);
+
+                Color color = colors[row][col];
+
+                if (color == null)
+                    grid[row][col].setBorder(null);
+                else
+                    grid[row][col].setBorder(BorderFactory.createLineBorder(color));
+            }
     }
 
-    private void rest()
+    /**
+     * 
+     * Waits for the user to select a move and returns this move.
+     * @return the seleceted move
+     */
+    public Move selectMove()
     {
         try
         {
-            Thread.sleep(500);
+            selectedPiece = null;
+            selectedMove = null;
+            while (selectedMove == null)
+                Thread.sleep(1);
+            Move move = selectedMove;
+            selectedPiece = null;
+            selectedMove = null;
+            return move;
         }
-        catch (InterruptedException e1)
+        catch(InterruptedException e)
         {
-            e1.printStackTrace();
-        }
-    }
-
-    private boolean makeTempMove(int column, Color c)
-    {
-        if (board.isValidMove(column))
-        {
-            board.makeTempMove(column, c);
-            repaint();
-            return true;
-        }
-        return false;
-    }
-
-    private boolean makeMove(int column)
-    {
-
-        if (board.isValidMove(column))
-        {
-            board.makeMove(column);
-            repaint();
-
-            // tests for a winner
-            Player winner = board.winner();
-            if (winner != null)
-            {
-                System.out.println("WINNER!");
-                gameIsOver = true;
-            }
-            
-            return true;
-        }
-        return false;
-    }
-
-    public void paintComponent(Graphics g)
-    {
-        System.out.println("repainting");
-
-        paintGrid(g);
-        paintPieces(g);
-        paintSidebar(g);
-    }
-
-    // TODO: WRITE THIS
-    // SHOULD HAVE: THE TWO PLAYERS - THEIR COLORS & NAMES. THE ONE WHOSE TURN IT IS
-    // SHOULD BE HIGHLIGHTED. ALSO HAVE A MAIN MENU BUTTON FOR RETURNING TO MAIN
-    // MENU
-    private void paintSidebar(Graphics g)
-    {
-        g.setColor(board.getPlayer1().getColor());
-        g.fillOval(640, 50, 70, 70);
-        g.setColor(board.getPlayer2().getColor());
-        g.fillOval(640, 175, 70, 70);
-
-        g.setColor(TEXT_COLOR);
-        g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
-        g.drawString(p1.name, 630, 30);
-        g.drawString(p2.name, 630, 155);
-
-        g.drawRect(630, 400, 85, 40);
-
-        g.drawRect(630, 460, 85, 40);
-
-        g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 22));
-        g.drawString("UNDO", 640, 429);
-        g.drawString("BACK", 643, 489);
-        // TODO: add restart "button"
-
-    }
-
-    /**
-     * Paints the entire screen the grid color first, then paints each empty
-     * "square" icon on.
-     * 
-     * @param g
-     */
-    private void paintGrid(Graphics g)
-    {
-        frame.getContentPane().setBackground(Color.WHITE);
-
-        g.setColor(Color.BLACK);
-
-        // adding in the empty slots
-        for (int c = 0; c < 19; c++)
-        {
-            for (int r = 0; r < 18; r++)
-            {
-                paintSlot(g, c, r, Color.BLACK);
-            }
+            e.printStackTrace();
+            System.exit(1);
+            return null;
         }
     }
 
     /**
-     * Paints a single slot in a given color, with provided row AND COLUMN.
      * 
-     * @param g     the graphics painter
-     * @param c     the column of the slot to be painted
-     * @param r     the row of the slot to be painted
-     * @param color
+     *Sets the title of the window.
+     *@param title the title
      */
-    private void paintSlot(Graphics g, int c, int r, Color color)
+    public void setTitle(String title)
     {
-        g.setColor(color);
-        g.drawRect(c * 75 + (c + 1) * 10, r * 75 + (r + 1) * 10, 75,75);
-    }
-
-    private void highlight(Graphics g, int c, int r, Color color)
-    {
-        g.setColor(color);
-        g.drawRoundRect(c * 75 + (c + 1) * 10, r * 75 + (r + 1) * 10, 75, 75, 45, 45);
-        g.drawRoundRect(c * 75 + (c + 1) * 10 + 1, r * 75 + (r + 1) * 10 + 1, 73, 73, 45, 45);
-        g.drawRoundRect(c * 75 + (c + 1) * 10 + 2, r * 75 + (r + 1) * 10 + 2, 71, 71, 45, 45);
-        g.drawRoundRect(c * 75 + (c + 1) * 10 + 3, r * 75 + (r + 1) * 10 + 3, 71, 71, 45, 45);
-        
+        frame.setTitle(title);
     }
 
     /**
-     * Goes through board.getPieces, and where there is a piece, use g to draw a
-     * circle with the piece's color.
      * 
-     * @param g the graphics painter
+     * Sets the color of the border for the given location, and redraws it.
+     * @param loc the location
+     * @param color the color
      */
-    private void paintPieces(Graphics g)
+    public void setColor(Location loc, Color color)
     {
-        Piece[][] piece = board.getPieces();
-
-        for (int c = 0; c < piece[0].length; c++)
-        {
-            for (int r = 0; r < piece.length; r++)
-            {
-                if (piece[r][c] != null)
-                {
-                    paintSlot(g, c, r, piece[r][c].getColor());
-                    if (piece[r][c].isHighlighted())
-                    {
-                        highlight(g, c, r, HIGHLIGHT_COLOR);
-                    }
-                }
-            }
-        }
+        colors[loc.getRow()][loc.getCol()] = color;
+        showBoard();
     }
 
     /**
-     * Returns the column of the game that the given x-coordinate would be in.
      * 
-     * @param x the x-coordinate
-     * @return the column
+     * Clears all border colors and redraws the board.
      */
-    private int getColumn(int x)
+    public void clearColors()
     {
-        if (x >= 10 && x <= 85)
-            return 0;
-        else if (x >= 95 && x <= 170)
-            return 1;
-        else if (x >= 180 && x <= 255)
-            return 2;
-        else if (x >= 265 && x <= 340)
-            return 3;
-        else if (x >= 350 && x <= 425)
-            return 4;
-        else if (x >= 435 && x <= 510)
-            return 5;
-        else if (x >= 520 && x <= 595)
-            return 6;
-        return -1;
+        for (int row = 0; row < colors.length; row++)
+            for (int col = 0; col < colors[row].length; col++)
+                colors[row][col] = null;
+        showBoard();
     }
 
 }
