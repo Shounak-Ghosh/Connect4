490c65864140e5cd4399b1a8cf31d5be51defb14
Shounak Ghosh <43559571+Shounak-Ghosh@users.noreply.github.com>
2019-05-29 17:19:00 -0700

    Use Connect4.java to...
    test changes to the actual game, without the main-menu code getting in the way.

--- Connect4.java
+++ Connect4.java
@@ -1,13 +1,22 @@
-
 package Connect4;
+
+import java.awt.Color;
 
 public class Connect4 
 {
     
+    
+    
     // driver class for the connect4 app
+    
+    // the code inside serves as a tester for the actual game, without the main-menu code in the way
     public static void main(String[] args) 
     {
-        // run the
+        Player d1; // defensive player
+        Display d;
+        
+        d = new BoardHandler(new HumanPlayer("p1", Color.YELLOW), new HumanPlayer("p2", Color.RED));
+        //d1 = new DefensivePlayer("def. player", Color.RED);
     }
 
 }
