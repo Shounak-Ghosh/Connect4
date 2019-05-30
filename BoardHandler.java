1ef169ecf476e938adeb46a80856c5d2d7ff86bb
Shounak Ghosh <43559571+Shounak-Ghosh@users.noreply.github.com>
2019-05-29 23:23:36 -0700

    added smartPlayer
    its a start

--- BoardHandler.java
+++ BoardHandler.java
@@ -7,6 +7,7 @@ import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -23,9 +24,10 @@ public class BoardHandler extends Display
     private int lastColumn;
     Player p1;
     Player p2;
-    
-    protected Board board;
+    Player humanPlayer;
+    Player computerPlayer;
 
+    protected Board board;
 
     private Clip dropNoise;
     private Clip buttonNoise;
@@ -40,10 +42,16 @@ public class BoardHandler extends Display
 
         // change this for digitalplayer
         // note: p2 will always be the computer bc we give human first turn
-        computerized = (p2 instanceof RandomPlayer || p2 instanceof DefensivePlayer);
+        computerized = (p2 instanceof RandomPlayer || p2 instanceof DefensivePlayer || p2 instanceof SmartPlayer);
+
+        if (computerized)
+        {
+            humanPlayer = p1;
+            computerPlayer = p2;
+        }
+
         lastColumn = -1;
-        
-        
+
         try
         {
             dropNoise = AudioSystem.getClip();
@@ -67,7 +75,7 @@ public class BoardHandler extends Display
             e.printStackTrace();
         }
     }
-    
+
     public Board getBoard()
     {
         return board;
@@ -128,9 +136,13 @@ public class BoardHandler extends Display
                 {
                     makeMove(defensivePlayerMove());
                 }
+                else if (p2 instanceof SmartPlayer)
+                {
+                    makeMove(smartPlayerMove());
+                }
                 // makeMove(randomPlayerMove());
                 // board.undo();
-                
+
             }
 
         }
@@ -162,15 +174,88 @@ public class BoardHandler extends Display
         return move;
     }
 
-    
-    private int smartPlayerMove() 
+    private int smartPlayerMove()
     {
-        return -1;
+        // HashSet<Integer> validColumns = new HashSet<Integer>();
+        HashSet<Integer> oneMoveWin = new HashSet<Integer>();
+        HashSet<Integer> twoMoveWin = new HashSet<Integer>();
+
+        for (int a = 0; a < 7; a++) // computer plays
+        {
+            board.makeTempMove(a, computerPlayer.getColor(), computerPlayer);
+            if (board.winner() != null && board.winner().equals(computerPlayer)) // win if a win is possible
+            {
+                board.clearTempMoves();
+                return a;
+            }
+            for (int b = 0; b < 7; b++) // human plays
+            {
+                board.makeTempMove(b, humanPlayer.getColor(), humanPlayer);
+                if (board.winner() != null && board.winner().equals(humanPlayer))
+                {
+                    oneMoveWin.add(b); // this strategy does not always work
+                    // add case where this move helps humanPlayer win
+                }
+                for (int c = 0; c < 7; c++) // computer plays
+                {
+                    board.makeTempMove(c, computerPlayer.getColor(), computerPlayer);
+                    for (int d = 0; d < 7; d++) // human plays
+                    {
+                        board.makeTempMove(d, humanPlayer.getColor(), humanPlayer);
+                        if (board.winner() != null && board.winner().equals(humanPlayer))
+                        {
+                            twoMoveWin.add(d);
+                        }
+                        board.clearTempMoves();
+                    }
+                    board.clearTempMoves();
+                }
+                board.clearTempMoves();
+            }
+            board.clearTempMoves();
+        }
+        board.clearTempMoves();
+
+        // flush out the invalid moves
+
+        for (Integer i : oneMoveWin)
+        {
+            if (!board.isValidMove(i))
+            {
+                oneMoveWin.remove(i);
+            }
+        }
+
+        for (Integer i : twoMoveWin)
+        {
+            if (!board.isValidMove(i))
+            {
+                twoMoveWin.remove(i);
+            }
+        }
+
+        // give priority to one move wins
+        if (!oneMoveWin.isEmpty())
+        {
+            for (Integer i : oneMoveWin)
+            {
+                return i;
+            }
+        }
+        
+        if(!twoMoveWin.isEmpty()) 
+        {
+            for (Integer i : twoMoveWin)
+            {
+                return i;
+            }
+        }
+        
+        return randomPlayerMove();
+        
+        
     }
-    
-    
-    
-    
+
     // TODO: make a twoInARow method in board (similar to winner) and use that
     // instead
     private int defensivePlayerMove()
@@ -246,17 +331,18 @@ public class BoardHandler extends Display
 //                e.printStackTrace();
 //            }
 //            
-            
+
             int finalRow = board.getTopmostEmptySlot(column);
             int row = 0;
-            double dropTime = 400;
-            while (row <= finalRow) 
+            double dropTime = 220;
+            while (row <= finalRow)
             {
                 board.animateMove(column, row);
                 paint(getGraphics());
-                // note: I used paint because repaint would only be called if the mouse is clicked again
-                //       instead of after every iteration of the while loop
-                //       this is why the pieces seem to appear and then disappear
+                // note: I used paint because repaint would only be called if the mouse is
+                // clicked again
+                // instead of after every iteration of the while loop
+                // this is why the pieces seem to appear and then disappear
                 System.out.println("should have repainted row " + row);
                 try
                 {
@@ -294,7 +380,6 @@ public class BoardHandler extends Display
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-            
 
             // tests for a winner
             Player winner = board.winner();
@@ -303,8 +388,7 @@ public class BoardHandler extends Display
                 System.out.println("WINNER!");
                 gameIsOver = true;
             }
-            
-            
+
             return true;
         }
         return false;
@@ -389,7 +473,7 @@ public class BoardHandler extends Display
         g.drawRoundRect(c * 75 + (c + 1) * 10 + 1, r * 75 + (r + 1) * 10 + 1, 73, 73, 45, 45);
         g.drawRoundRect(c * 75 + (c + 1) * 10 + 2, r * 75 + (r + 1) * 10 + 2, 71, 71, 45, 45);
         g.drawRoundRect(c * 75 + (c + 1) * 10 + 3, r * 75 + (r + 1) * 10 + 3, 71, 71, 45, 45);
-        
+
     }
 
     /**
