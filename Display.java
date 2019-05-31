1e96272612482f1a9d4d3f24f49d0bc6a1104a66
Shounak Ghosh <43559571+Shounak-Ghosh@users.noreply.github.com>
2019-05-31 12:51:01 -0700

    exits on close

--- Display.java
+++ Display.java
@@ -22,13 +22,13 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 /**
- * The abstract superclass for all displays in the project.
- * Makes setting sizes, behavior, keeping constant colors,
- * determining insets, etc. easier.
- * Also is in charge of storing game information and reading 
- * it back in from the text file (since all displays will need
- * to do these methods)
+ * The abstract superclass for all displays in the project. Makes setting sizes,
+ * behavior, keeping constant colors, determining insets, etc. easier. Also is
+ * in charge of storing game information and reading it back in from the text
+ * file (since all displays will need to do these methods)
+ * 
  * @author Gloria Zhu
+ * @author Shounak Ghosh
  *
  */
 public abstract class Display extends JComponent implements MouseListener
@@ -36,30 +36,32 @@ public abstract class Display extends JComponent implements MouseListener
     // not sure what this is for, but I guess it's required
     // for extending a jcomponent
     private static final long serialVersionUID = 1L;
-    
+
     protected JFrame frame;
 
     protected final Color BACKGROUND_COLOR = new Color(192, 192, 192);
-    protected final Color GRID_COLOR = new Color(125,125,125);
+    protected final Color GRID_COLOR = new Color(125, 125, 125);
     protected final Color HIGHLIGHT_COLOR = Color.black;
     protected final Color TEXT_COLOR = Color.WHITE;
 
     // insets of the frame (platform-dependent)
     protected Insets insets;
-    
+
     // static variables (same for all displays)
     public static ArrayList<Game> games;
     protected static Display mainMenu;
 
-    
-    public Display() {
+    public Display()
+    {
         frame = new JFrame();
-        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // stores games upon exiting
-        WindowListener exitListener = new WindowAdapter() {
+        WindowListener exitListener = new WindowAdapter()
+        {
 
             @Override
-            public void windowClosing(WindowEvent e) {
+            public void windowClosing(WindowEvent e)
+            {
                 Noise.playButtonNoise();
                 Noise.stopMenuMusic();
                 storeGames();
@@ -67,21 +69,22 @@ public abstract class Display extends JComponent implements MouseListener
         };
         frame.addWindowListener(exitListener);
     }
-    
-    public ArrayList<Game> getGames() {
+
+    public ArrayList<Game> getGames()
+    {
         return games;
     }
-    
-    
+
     // storing/reading in games
-    
-    public void storeGames() {
+
+    public void storeGames()
+    {
         String workingDir = System.getProperty("user.dir");
 
-        Path filePath = Paths.get(workingDir+File.separator+"sampleFile.txt");
-        
+        Path filePath = Paths.get(workingDir + File.separator + "sampleFile.txt");
+
         File file = new File(filePath.toString());
-        
+
         try
         {
             // wiping the file
@@ -90,135 +93,148 @@ public abstract class Display extends JComponent implements MouseListener
             wiper.close();
 
             file.createNewFile();
-            
+
             BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
-            
+
             System.out.println(games);
-            for (int i=games.size()-1;i>=0;i--) {
-                
+            for (int i = games.size() - 1; i >= 0; i--)
+            {
+
                 Game g = games.get(i);
-                
+
                 // writing 0 for human player & the player's name/color
                 writer.write("0");
                 writer.write(g.getPlayer1().toString());
                 writer.write(";");
                 writer.write(String.valueOf(g.getPlayer1().getColor().getRGB()));
                 writer.newLine();
-                
+
                 // writing the player's type, name, and color
                 Player player2 = g.getPlayer2();
-                if (player2 instanceof RandomPlayer) writer.write("1");
-                else if (player2 instanceof DefensivePlayer) writer.write("2");
+                if (player2 instanceof RandomPlayer)
+                    writer.write("1");
+                else if (player2 instanceof DefensivePlayer)
+                    writer.write("2");
 //                else if (player2 instanceof SmartPlayer) writer.write("3");
                 else writer.write("0");
                 writer.write(g.getPlayer2().toString());
                 writer.write(";");
                 writer.write(String.valueOf(player2.getColor().getRGB()));
                 writer.newLine();
-                
+
                 Stack<Integer> moveStack = g.getBoardHandler().getBoard().getMoves();
                 ArrayList<Integer> moves = new ArrayList<Integer>(moveStack);
-                for (int j=0;j<moves.size();j++) {
-                    writer.write(String.valueOf(moves.get(moves.size()-1-j)));
+                for (int j = 0; j < moves.size(); j++)
+                {
+                    writer.write(String.valueOf(moves.get(moves.size() - 1 - j)));
                 }
                 writer.newLine();
             }
             writer.close();
-        } catch (IOException e)
+        }
+        catch (IOException e)
         {
             e.printStackTrace();
         }
-        
-        System.out.println("games stored: "+games);
-        
-        
+
+        System.out.println("games stored: " + games);
+
     }
-    
-    public void loadGames() {
+
+    public void loadGames()
+    {
         games = new ArrayList<Game>();
-        try {
+        try
+        {
             String workingDir = System.getProperty("user.dir");
 
-            Path filePath = Paths.get(workingDir+File.separator+"sampleFile.txt");
+            Path filePath = Paths.get(workingDir + File.separator + "sampleFile.txt");
 
             File file = new File(filePath.toString());
 
-            if (file.createNewFile()) return;
+            if (file.createNewFile())
+                return;
 
             BufferedReader br = new BufferedReader(new FileReader(file.getAbsolutePath()));
 
             ArrayList<String> player1name = new ArrayList<String>();
             ArrayList<Color> player1color = new ArrayList<Color>();
-            
+
             ArrayList<Integer> player2type = new ArrayList<Integer>();
             ArrayList<String> player2name = new ArrayList<String>();
             ArrayList<Color> player2color = new ArrayList<Color>();
-            
+
             ArrayList<ArrayList<Integer>> moves = new ArrayList<ArrayList<Integer>>();
 
             int count = 0;
-            for (String line; (line = br.readLine()) != null;) {
-                if (count==6) break;
+            for (String line; (line = br.readLine()) != null;)
+            {
+                if (count == 6)
+                    break;
                 String player1info = line;
-                process(player1name, player1color,player1info);
+                process(player1name, player1color, player1info);
                 String player2info = br.readLine();
-                process(player2name,player2color,player2info,player2type);
-                
+                process(player2name, player2color, player2info, player2type);
+
                 moves.add(new ArrayList<Integer>());
-                
+
                 String moveSet = br.readLine();
-                for(int i=0;i<moveSet.length();i++) {
-                    int move = Integer.valueOf(moveSet.substring(i,i+1));
-                    System.out.println("pushed move "+move+" to i");
-                    moves.get(count).add(move);         
-                 }
+                for (int i = 0; i < moveSet.length(); i++)
+                {
+                    int move = Integer.valueOf(moveSet.substring(i, i + 1));
+                    System.out.println("pushed move " + move + " to i");
+                    moves.get(count).add(move);
+                }
                 count++;
             }
-            
+
             br.close();
-            
-            for(int i=0;i<count;i++) {
-                Player p1 = new HumanPlayer(player1name.get(i),
-                                       player1color.get(i));
-                Player p2 = new HumanPlayer(player2name.get(i),
-                        player2color.get(i)); // change this after getting smart player
+
+            for (int i = 0; i < count; i++)
+            {
+                Player p1 = new HumanPlayer(player1name.get(i), player1color.get(i));
+                Player p2 = new HumanPlayer(player2name.get(i), player2color.get(i)); // change this after getting smart
+                                                                                      // player
                 int p2type = player2type.get(i);
-                if (p2type==0) p2 = new HumanPlayer(player2name.get(i),
-                                        player2color.get(i));
-                else if (p2type==1) p2 = new RandomPlayer(player2name.get(i),
-                                        player2color.get(i));
-                else if (p2type==2) p2 = new DefensivePlayer(player2name.get(i),
-                                        player2color.get(i));
+                if (p2type == 0)
+                    p2 = new HumanPlayer(player2name.get(i), player2color.get(i));
+                else if (p2type == 1)
+                    p2 = new RandomPlayer(player2name.get(i), player2color.get(i));
+                else if (p2type == 2)
+                    p2 = new DefensivePlayer(player2name.get(i), player2color.get(i));
 //                else if (p2type==3) p2 = new SmartPlayer(player2name.get(i),
-                        
-                games.add(new Game(p1,p2,moves.get(i)));
+
+                games.add(new Game(p1, p2, moves.get(i)));
             }
-            
-        } catch (Exception e) {
+
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
-        
-        System.out.println("games loaded: "+games);
+
+        System.out.println("games loaded: " + games);
     }
-    
 
     // process 1 line of player info
-    
-    private void process(ArrayList<String> names, ArrayList<Color> colors, String info, ArrayList<Integer> types) {
+
+    private void process(ArrayList<String> names, ArrayList<Color> colors, String info, ArrayList<Integer> types)
+    {
         process(names, colors, info);
         types.add(0, Integer.valueOf(info.substring(0, 1)));
     }
 
-    private void process(ArrayList<String> names, ArrayList<Color> colors, String info) {
+    private void process(ArrayList<String> names, ArrayList<Color> colors, String info)
+    {
         int semi = info.indexOf(";");
         names.add(0, info.substring(1, semi));
         colors.add(0, new Color(Integer.valueOf(info.substring(semi + 1))));
 
-}
+    }
 
-    
     // display itself
-    protected void displaySelf() {
+    protected void displaySelf()
+    {
         frame.getContentPane().add(this); // very important line of code reee
 
         frame.setResizable(false);
@@ -233,26 +249,42 @@ public abstract class Display extends JComponent implements MouseListener
 
         frame.setVisible(true);
 
-        // removing previous mouse listeners so mouse events 
+        // removing previous mouse listeners so mouse events
         // aren't triggered twice
-        for(int i=0;i<frame.getMouseListeners().length;i++) {
+        for (int i = 0; i < frame.getMouseListeners().length; i++)
+        {
             frame.removeMouseListener(frame.getMouseListeners()[i]);
         }
         frame.addMouseListener(this);
 
         repaint();
     }
-    
+
     // close itself
-    public void closeSelf() {
+    public void closeSelf()
+    {
         storeGames();
         frame.dispose();
     }
 
     // this method should be overridden
-    public void mouseClicked(MouseEvent e) {}
-    public void mousePressed(MouseEvent e) {}
-    public void mouseReleased(MouseEvent e) {}
-    public void mouseEntered(MouseEvent e) {}
-    public void mouseExited(MouseEvent e) {}
+    public void mouseClicked(MouseEvent e)
+    {
+    }
+
+    public void mousePressed(MouseEvent e)
+    {
+    }
+
+    public void mouseReleased(MouseEvent e)
+    {
+    }
+
+    public void mouseEntered(MouseEvent e)
+    {
+    }
+
+    public void mouseExited(MouseEvent e)
+    {
+    }
 }
